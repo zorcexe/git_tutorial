6d4c61fea589f9294fcd6b534eda0f0906b6fc98
Max Mustermann <max@example.com>
2026-06-01 12:11:15 +0200

    First Commit

---
+++ git_tutorial.md
@@ -0,0 +1,16 @@
+# Git Tutorial
+Die wichtigsten Git-Befehle 
+
+## neues Git Repositry initialisieren
+
+    git init
+
+=> legt .git-Verzeichnis an
+
+## den aktuell Projektstand ausgeben
+
+    git status
+
+## DAteien auf die Staging (Bühne) bringen
+
+    git add .
---
+++ hello.py
@@ -0,0 +1,21 @@
+""" 
+Ein Python Programm.
+
+Software-Versionierungssysteme (zb. Git, Mercurial, SVN, CVS...)
+
+Git löst das Problem:
+- Wann wurde was von wem geändert? zb. Hinzugefügt, Gelöscht, Verändert (Historie)
+- Das Code aus verschiedenen Zuständen zusammengeführt (merge)
+
+
+# Git funktioniert projektbasiert (ihr habt ein Projektverzeichnis und daraus wird ein git Projekt gemacht, Repository)
+git init 
+
+
+"""
+def hello():
+    print("hello, world")
+
+
+def good():
+    print("good bye from Alice")
